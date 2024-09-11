cb1d77b47e4b97b1f22e7946dbca90952c372509
Ajay-Lobo <nishanthajay1101@gmail.com>
2024-09-12 00:28:51 +0530

    gemini integration with nodejs

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-// Make sure to include these imports:
+import express from "express";
+import bodyParser from "body-parser";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
+const app = express();
 dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Write a story about a magic backpack.";
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("index", { response: null });
+});
 
+app.post("/ask", async (req, res) => {
+  const userQuestion = req.body.question;
 
-const generateContent = async()=>{
-    try{
-        const result = await model.generateContent(prompt);
-        console.log(result.response.text());
-    }catch(err){
-        console.log(err)
-    }
-}
+  if (!userQuestion) {
+    return res.render("index", { response: "Please provide a question!!!." });
+  }
 
-generateContent();
+  try {
+    const result = await model.generateContent(userQuestion);
+
+    res.render("index", { response: result.response.text() });
+  } catch (err) {
+    console.error("Error from API:", err.message);
+    res.render("index", {
+      response: "Sorry, something went wrong. Try again later.",
+    });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
